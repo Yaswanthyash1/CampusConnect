9159b6452c872d00200c77d23be4c5397adcb15b
Yaswanth Namburi <yaswanthnamburi1010@gmail.com>
2025-08-27 14:01:15 +0530

    Initial commit

---
+++ src/main/java/com/collegeclubs/club_management/ClubManagementApplication.java
@@ -0,0 +1,15 @@
+package com.collegeclubs.club_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagementApplication.class, args);
+
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ src/test/java/com/collegeclubs/club_management/ClubManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeclubs.club_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
